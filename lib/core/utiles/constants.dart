0f0a5523d740829e0a6195b755ebdbc5749c38dd
mostafa <mustafa.bahr12323@gmail.com>
2023-10-24 10:48:44 +0300

    showErrorDialog in AppConstants

---
+++ lib/core/utiles/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppConstants
+{
+
+  static void showErrorDialog({
+  required String message,
+  required BuildContext context,
+})
+  {
+    showDialog(context: context, builder: (context){
+      return CupertinoAlertDialog(
+        title: Text(message),
+        actions: [
+          TextButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: const Text("ok"),
+          ),
+        ],
+      );
+    });
+  }
+
+
+}
