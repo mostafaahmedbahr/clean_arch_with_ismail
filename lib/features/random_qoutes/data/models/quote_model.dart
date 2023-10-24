9725b85bf580d0efd1a2eca7405a7b60cfe67580
mostafa <mustafa.bahr12323@gmail.com>
2023-10-24 12:37:47 +0300

    QuoteModel

---
+++ lib/features/random_qoutes/data/models/quote_model.dart
@@ -0,0 +1,16 @@
+import 'package:clean_arch_with_ismail/features/random_qoutes/domain/entities/quote_entity.dart';
+
+class QuoteModel extends QuoteEntity {
+  const QuoteModel(
+      {required super.content,
+      required super.autherName,
+      required super.date,
+      required super.id});
+
+  factory QuoteModel.fromJson(Map<String, dynamic> json) => QuoteModel(
+      content: json['content'],
+      autherName: json['author'],
+      date: json['dateAdded'],
+      id: json['id'],
+  );
+}
